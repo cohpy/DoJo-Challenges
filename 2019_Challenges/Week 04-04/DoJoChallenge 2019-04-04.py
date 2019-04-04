1ffedf91828db54c7906b43f50a7d58ba5ddd310
Travis Risner <deeppunster@gmail.com>
2019-04-04 15:56:48 -0400

    Added DoJo Challenges for April 4, 2019.

---
+++ 2019_Challenges/Week 04-04/DoJoChallenge 2019-04-04.py
@@ -0,0 +1,12 @@
+"""
+DoJo Challenge for April 4, 2019.  (Easy)
+
+A number is considered perfect if its digits sum up to exactly 10.
+
+Given a positive integer n, return the n-th perfect number.
+
+For example, given 1, you should return 19. Given 2, you should return 28.
+
+
+Source:  Daily Coding Problems (https://www.dailycodingproblem.com/)
+"""
